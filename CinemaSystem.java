1d37c8dcb09ff490a3efcfa7681d185d3878092a
d44-eng <salamehdina114@gmail.com>
2026-03-14 05:38:58 +0300

    Update CinemaSystem.java

--- CinemaSystem.java
+++ CinemaSystem.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package cinemasystem;
 
-/**
- *
- * @author salam_u8r3zye
- */
+
 public class CinemaSystem {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
